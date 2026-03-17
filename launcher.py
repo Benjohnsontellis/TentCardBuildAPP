e0bab6f36f46750f124305a1b22df570265a0896
Benjohnsontellis <benjohnsontellis@gmail.com>
2026-03-17 19:48:28 +0530

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -5,30 +5,26 @@ import sys
 import os
 
 
-
 def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
 
-    return os.path.join(base_path, relative_path)
-
+    return os.path.join(base_path, relative_path)
 
 
 app_path = resource_path("app.py")
 
 
-
 subprocess.Popen([
-    sys.executable,
-    "-m",
-    "streamlit",
-    "run",
-    app_path,
-    "--server.headless=true"
+    sys.executable,
+    "-m",
+    "streamlit",
+    "run",
+    app_path,
+    "--server.headless=true"
 ])
-
 
 
 time.sleep(4)
